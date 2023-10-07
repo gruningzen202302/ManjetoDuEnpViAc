df057ad078cf0e16cfcc287614986c31e2932eaa
vir-th-u23 <alberto.gruning.zen@gmail.com>
2023-10-07 03:22:29 -0300

    chore:update gradle to compile ok

--- app/src/main/java/com/qimono/manjetoduenpviac/MainActivity.kt
+++ app/src/main/java/com/qimono/manjetoduenpviac/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.qimono.manjetoduenpviac
 
-import androidx.appcompat.app.AppCompatActivity
+//import com.qimono.manjetoduenpviac.ui.theme
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+//import androidx.activity.compose.rememberLauncherForActivityResult
+//import androidx.activity.compose.setContent
+//import androidx.activity.result.contract.ActivityResultContracts
+//import androidx.compose.runtime.collectAsState
+//import androidx.compose.runtime.getValue
+//import androidx.hilt.navigation.compose.hiltViewModel
+//import com.qimono.manjetoduenpviac.ui.VideoPlayerComposeTheme
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContent{
+//            VideoPlayerComposeTheme{
+//                val viewModel = hiltViewModel<MainViewModel>()
+//                val videoItems by viewModel.videoItems.collectAsState()
+//                val selectVideoLauncher = rememberLauncherForActivityResult(
+//                    contract = ActivityResultContracts.GetContent(),
+//                    onResult = {uri->
+//                        uri?.let(viewModel::addVideoUri)
+//                    }
+//                )
+//            }
+//        }
     }
 }
---
+++ app/src/main/java/com/qimono/manjetoduenpviac/ui/theme.kt
@@ -0,0 +1,14 @@
+package com.qimono.manjetoduenpviac.ui
+
+
+import androidx.compose.runtime.Composable
+
+
+
+
+
+@Composable
+fun VideoPlayerComposeTheme(
+    darkTheme: Boolean = true,//isSystemInDarkTheme(),
+    content: @Composable()Any
+){}
